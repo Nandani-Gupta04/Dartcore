9d3e2ed90fbbe6ab4830a061c58d266b75153f3b
Nandani-Gupta04 <nandani9904gupta@gmail.com>
2026-05-07 20:05:28 +0530

    Add files via upload
    This is a simple to do app for managing daily tasks.

---
+++ To Do App/database/database.dart
@@ -0,0 +1,21 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class TodoDataBase{
+  List TodoList=[];
+final _mybox=Hive.box('mybox');
+
+void createData(){
+  TodoList=[
+        ["TODO",false],
+
+  ];
+}
+void loadData(){
+  TodoList=_mybox.get("TODOLIST");
+}
+void updateData(){
+  _mybox.put("TODOLIST", TodoList);
+}
+
+
+}
